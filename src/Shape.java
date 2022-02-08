7f92f22b24f18e86b1713a2178fb65dc01faa3a2
SkyttRPS <marcusskytt@live.dk>
2022-02-08 03:53:11 +0100

    First build

--- src/Shape.java
+++ src/Shape.java
@@ -1,6 +1,11 @@
+/*
+ *  A class Shape that holds the x and y positions of any shape.
+ *  The class implements the ShapeMethods interface.
+ */
+
 public abstract class Shape implements ShapeMethods{
-    private double xPos;
-    private double yPos;
+    private double xPos; // To hold the x position of the shape
+    private double yPos; // TO hold the y position of the shape
 
     public Shape(double xPos, double yPos) {
         this.xPos = xPos;
--- src/ShapeMethods.java
+++ src/ShapeMethods.java
@@ -1,6 +1,13 @@
 public interface ShapeMethods {
 
     public double computeArea();
-    public double computeCircumfrence();
+    public double computeCircumference();
     public double returnCenter();
+    public double returnDistance();
+
+    /* Future behavior:
+    - tell if one shape is inside another shape
+    - compute overlaps between shapes
+    - move shapes within a two-dimensional space
+    - layout shapes without any overlaps */
 }
