7d2229978488ee98b3b2af0f6d3862141a2bf67d
SkyttRPS <marcusskytt@live.dk>
2022-02-08 03:32:47 +0100

    Added Shape Superclass
    Added shape superclass with x and y positional parameters

---
+++ src/Shape.java
@@ -0,0 +1,9 @@
+public class Shape {
+    private double xPos;
+    private double yPos;
+
+    public Shape(double xPos, double yPos) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+}
